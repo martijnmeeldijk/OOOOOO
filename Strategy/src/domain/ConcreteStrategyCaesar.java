14d76279975ab4d3941f0c2301fa70dc8ccf172d
Martijn <martijn.meeldijk@student.ucll.be>
2019-09-26 17:52:38 +0200

    Caesar aangepast om interface te implementeren

--- Strategy/src/domain/ConcreteStrategyCaesar.java
+++ Strategy/src/domain/ConcreteStrategyCaesar.java
@@ -1,4 +1,20 @@
 package domain;
 
-public class ConcreteStrategyCaesar {
+public class ConcreteStrategyCaesar implements CodingStrategy {
+    String message;
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public ConcreteStrategyCaesar(String message){
+        this.message = message;
+    }
+
+    @Override
+    public String decodeer(String message) {
+        return null;
+    }
+
+    @Override
+    public String codeer(String message) {
+        return null;
+    }
 }
