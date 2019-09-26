271c3b19ed95f4da1b806fed7bd57ca67d4e8998
Maarten <maarten-spam@hotmail.com>
2019-09-26 17:44:41 +0200

    codeer UI

--- Strategy/src/ui/CodeerUi.java
+++ Strategy/src/ui/CodeerUi.java
@@ -1,4 +1,11 @@
 package ui;
 
+import domain.CodeerContext;
+
 public class CodeerUi {
+    private CodeerContext codeerContext;
+
+    public CodeerUi(CodeerContext codeerContext) {
+        this.codeerContext = codeerContext;
+    }
 }
