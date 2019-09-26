8ef0f1d01e9f8daf642de51a7bca9b31eace5ed8
Maarten <maarten-spam@hotmail.com>
2019-09-26 17:40:50 +0200

    coding context

--- Strategy/src/domain/CodeerContext.java
+++ Strategy/src/domain/CodeerContext.java
@@ -1,4 +1,20 @@
 package domain;
 
 public class CodeerContext {
+    private String text;
+    private  CodingStrategy codingStrategy;
+
+    public CodeerContext(String text) {
+        this.text = text;
+
+
+    }
+
+    public String Codeer(){
+        return null;
+
+    }
+    public String Decodeer(){
+        return null;
+    }
 }
