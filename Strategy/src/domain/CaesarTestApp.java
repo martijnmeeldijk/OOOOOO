03dadbb2e71e5e420504a9306e4d58cfb9e168e3
Martijn <martijn.meeldijk@student.ucll.be>
2019-09-30 15:55:59 +0200

    gitignore geupdated

--- Strategy/src/domain/CaesarTestApp.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public class CaesarTestApp {
-    public static void main(String[] args){
-        ConcreteStrategyCaesar caesar = new ConcreteStrategyCaesar(5);
-        System.out.println(caesar.codeer("poep"));
-    }
-}
