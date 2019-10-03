2af50d585502f2b88f8b5d8edecf981410e3f90f
Bram <bram.verbist@student.ucll.be>
2019-10-03 18:25:27 +0200

    constructor Materiaal

--- Labo3/src/domain/Materiaal.java
+++ Labo3/src/domain/Materiaal.java
@@ -7,4 +7,12 @@ public class Materiaal {
     private MateriaalState uitgeleend;
     private MateriaalState beschadigd; // zoals Maarten
 
+    public Materiaal(){
+        this.verwijderd = new Verwijderd();
+        this.uitgeleend = new Uitgeleend();
+        this.uitleenbaar = new Uitleenbaar();
+        this.beschadigd = new Beschadigd();
+        this.state = uitleenbaar;
+    }
+
 }
