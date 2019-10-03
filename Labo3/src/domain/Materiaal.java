fab9fd38867b0fe2669ac7d59724ee5bd53503b6
Bram <bram.verbist@student.ucll.be>
2019-10-03 18:21:22 +0200

    var Materiaal

--- Labo3/src/domain/Materiaal.java
+++ Labo3/src/domain/Materiaal.java
@@ -1,4 +1,10 @@
 package domain;
 
 public class Materiaal {
+    private MateriaalState state;
+    private MateriaalState verwijderd;
+    private MateriaalState uitleenbaar;
+    private MateriaalState uitgeleend;
+    private MateriaalState beschadigd; // zoals Maarten
+
 }
