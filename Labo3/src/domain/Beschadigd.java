47ff71bbaf7d0aa349a4848facf47a473ccd85e1
Maarten <maarten-spam@hotmail.com>
2019-10-11 14:56:43 +0200

    added methodes in states

--- Labo3/src/domain/Beschadigd.java
+++ Labo3/src/domain/Beschadigd.java
@@ -2,4 +2,11 @@ package domain;
 
 public class Beschadigd extends MateriaalState {
 
+    public Beschadigd(Materiaal marteriaal) {
+        super(marteriaal);
+    }
+    @Override
+    void verwijder() {
+        super.verwijder();
+    }
 }
