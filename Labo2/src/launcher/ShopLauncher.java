8f65024f68d23da9b9afebdd55a05075e15484a7
Bram <bram.verbist@student.ucll.be>
2019-10-17 15:30:42 +0200

    stopt bij error

--- Labo2/src/launcher/ShopLauncher.java
+++ Labo2/src/launcher/ShopLauncher.java
@@ -9,17 +9,17 @@ public class ShopLauncher {
     public static CodeerContext codeerContext = new CodeerContext("codingcontext", null);
     public static void main(String[] args) {
         String methode = CodeerUi.drawMainMenu();
+        switch (methode){
+            case "1": codeerContext.setCodingStrategy(new ConcreteStrategyCaesar(2));
+            break;
+            case "2":codeerContext.setCodingStrategy(new ConcreteStrategySpiegel());
+            break;
+            case "0":break;
+            default: throw new IllegalArgumentException();
+
+        }
         String deorenc = CodeerUi.codingOrEncoding();
         String text = CodeerUi.showString();
-        if(methode.equals("1")){//kies concrete strat
-            codeerContext.setCodingStrategy(new ConcreteStrategyCaesar(2));
-        }
-        else if(methode.equals("2")){
-            codeerContext.setCodingStrategy(new ConcreteStrategySpiegel());
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
         if(deorenc.equals("1")){//choose coding or decoding
             codeerContext.setText(text);
             codeerContext.codeer();
