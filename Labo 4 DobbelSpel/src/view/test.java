8322328162927d0d755856197baad8b954a6ce6b
Bram <bram.verbist@student.ucll.be>
2019-10-17 21:07:34 +0200

    speler1 kan dobbelen

---
+++ Labo 4 DobbelSpel/src/view/test.java
@@ -0,0 +1,11 @@
+package view;
+
+public class test {
+    public static void main(String args[]){
+        Dobbel d = new Dobbel();
+        System.out.println("beurt 1:" +d.dobbel());
+        System.out.println("beurt 2:" +d.dobbel());
+        System.out.println("beurt 3:" +d.dobbel());
+        System.out.println("beurt 4:" +d.dobbel());
+    }
+}
