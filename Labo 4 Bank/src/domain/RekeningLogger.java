240ff9e5bc879f214561ad6acfc0a46d418ef594
Maarten <maarten-spam@hotmail.com>
2019-10-17 16:33:51 +0200

    update logger

--- Labo 4 Bank/src/domain/RekeningLogger.java
+++ Labo 4 Bank/src/domain/RekeningLogger.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 
 public class RekeningLogger implements Observer{
@@ -10,6 +11,9 @@ public class RekeningLogger implements Observer{
 
     @Override
     public void update(LinkedHashMap<String, Rekening> rekeningen) {
-
+        Object[] rekeningen1 = rekeningen.entrySet().toArray();
+        Rekening rekening1 =(Rekening) rekeningen1[rekeningen1.length -1];
+        System.out.println("Nummer " + rekening1.getNummer() + "\nsaldo= " + rekening1.getSaldo() +"\n"+ LocalDateTime.now().toString());
+        System.out.println("Het aantal rekeningen = " + rekeningen1.length);
     }
 }
