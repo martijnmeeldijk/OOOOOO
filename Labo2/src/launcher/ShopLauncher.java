4dddf8674ae5dc24ca3984dc7efc733bc83d20ee
Bram <bram.verbist@student.ucll.be>
2019-10-17 15:57:09 +0200

    loopt, is stopbaar en cancel werk

--- Labo2/src/launcher/ShopLauncher.java
+++ Labo2/src/launcher/ShopLauncher.java
@@ -8,31 +8,36 @@ import ui.CodeerUi;
 public class ShopLauncher {
     public static CodeerContext codeerContext = new CodeerContext("codingcontext", null);
     public static void main(String[] args) {
-        String methode = CodeerUi.drawMainMenu();
-        switch (methode){
-            case "1": codeerContext.setCodingStrategy(new ConcreteStrategyCaesar(2));
-            break;
-            case "2":codeerContext.setCodingStrategy(new ConcreteStrategySpiegel());
-            break;
-            case "0":break;
-            default: throw new IllegalArgumentException();
+        boolean stop = false;
+        while(!stop){
+            String methode = CodeerUi.drawMainMenu();
+            if(methode == null || methode.equals("0")) stop=true;
+            else if(methode.equals("1")) codeerContext.setCodingStrategy(new ConcreteStrategyCaesar(2));
+            else if(methode.equals("2")) codeerContext.setCodingStrategy(new ConcreteStrategySpiegel());
+            else throw new IllegalArgumentException();
 
+            if(!stop){
+                String deorenc = CodeerUi.codingOrEncoding();
+                if(deorenc != null){
+                    if(deorenc.equals("1")){//choose coding or decoding
+                        String text = CodeerUi.showString();
+                        if(text!=null){
+                            codeerContext.setText(text);
+                            codeerContext.codeer();
+                            CodeerUi.showString(codeerContext.getResult());
+                        }
+                    }
+                    else if(deorenc.equals("2")){//choose coding or decoding
+                        String text = CodeerUi.showString();
+                        if(text!=null){
+                            codeerContext.setText(text);
+                            codeerContext.decodeer();
+                            CodeerUi.showString(codeerContext.getResult());
+                        }
+                    }
+                    else throw new IllegalArgumentException();
+                }
+            }
         }
-        String deorenc = CodeerUi.codingOrEncoding();
-        String text = CodeerUi.showString();
-        if(deorenc.equals("1")){//choose coding or decoding
-            codeerContext.setText(text);
-            codeerContext.codeer();
-            CodeerUi.showString(codeerContext.getResult());
-        }
-        else if(deorenc.equals("2")){//choose coding or decoding
-            codeerContext.setText(text);
-            codeerContext.decodeer();
-            CodeerUi.showString(codeerContext.getResult());
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
-
     }
 }
