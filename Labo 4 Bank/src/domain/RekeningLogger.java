db88f2896627ff96af83866d709d76666a4ddcae
Maarten <maarten-spam@hotmail.com>
2019-10-17 16:32:11 +0200

    update logger

--- Labo 4 Bank/src/domain/RekeningLogger.java
+++ Labo 4 Bank/src/domain/RekeningLogger.java
@@ -1,12 +1,15 @@
 package domain;
 
+import java.util.LinkedHashMap;
+
 public class RekeningLogger implements Observer{
 
     public RekeningLogger() {
     }
 
+
     @Override
-    public void update(Rekening rekening) {
+    public void update(LinkedHashMap<String, Rekening> rekeningen) {
 
     }
 }
