660a62caab7842f77a55fefc5a4a882972d50dab
Martijn <martijn.meeldijk@student.ucll.be>
2019-10-17 15:30:17 +0200

    bank edit

--- Labo 4 Bank/src/domain/Bank.java
+++ Labo 4 Bank/src/domain/Bank.java
@@ -2,5 +2,16 @@ package domain;
 
 import java.util.Map;
 
+
 public class Bank {
+    private Map<String, Rekening> lijst;
+
+    public Bank(Map<String, Rekening> lijst) {
+        this.lijst = lijst;
+    }
+
+    public void openRekening(Rekening rekening){
+        if(rekening == null) throw new IllegalArgumentException("penis");
+        lijst.put(rekening.getNummer(), rekening);
+    }
 }
