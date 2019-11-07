e90c495695263f4cfe3940122981245143702948
Bram <bram.verbist@student.ucll.be>
2019-11-07 18:03:39 +0100

    wtf is going on

--- StartCodeDobbelSpel/scr/view/PlayerView.java
+++ StartCodeDobbelSpel/scr/view/PlayerView.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 public class PlayerView implements Observer {
 	private Stage stage = new Stage();
 	private Speler speler;
